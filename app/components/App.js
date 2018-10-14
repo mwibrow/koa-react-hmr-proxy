504afcc1b8a9e1e2d5f4d92981f1ea33af2c422c
mwibrow <mwibrow@gmail.com>
2018-10-14 17:27:19 +0100

    Import correct middleware

--- app/components/App.js
+++ app/components/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 class App extends React.Component {
   render () {
     return <div className='banner'>
-      It works!
+      <h1>It works! :)</h1>
     </div>
   }
 }
--- server/server-hmr.js
+++ server/server-hmr.js
@@ -2,10 +2,39 @@ import http from 'http'
 import Koa from 'koa'
 import path from 'path'
 import staticCache from 'koa-static-cache'
+import webpack from 'webpack'
+import { devMiddleware, hotMiddleware } from 'koa-webpack-middleware'
 
 import config from './config'
+import { appConfig } from '../webpack.config.app.babel'
 
 const app = new Koa()
+
+const compiler = webpack(appConfig)
+
+app.use(
+  devMiddleware(compiler, {
+    index: false,
+    noInfo: false,
+    quiet: false,
+    lazy: false,
+    watchOptions: {
+      aggregateTimeout: 500,
+      poll: true
+    },
+    publicPath: appConfig.output.publicPath,
+    stats: {
+      colors: true
+    },
+    timeout: 1000 * 20
+  })
+)
+app.use(
+  hotMiddleware(compiler, {
+    path: '/__webpack_hmr_app',
+    heartbeat: 10 * 1000
+  })
+)
 
 app.use(
   staticCache(path.join(__dirname, '/public/'), {
